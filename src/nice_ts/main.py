ac4f4ebec584bae3163597ddbbca9e4715d2ff00
Liad Magen <liad.magen@edu.fh-campuswien.ac.at>
2025-04-11 13:33:37 +0000

    new

---
+++ src/nice_ts/main.py
@@ -0,0 +1,6 @@
+hello = "Hello, World!"
+print(hello)
+'''A virtual environment in Python is used to create an isolated environment for your project. It ensures that dependencies and packages installed for one project do not interfere with others. It is particularly useful when working on multiple projects with different dependencies or package versions. In your case, you can use the virtual environment to install and manage Python packages specific to your nice_TS project without affecting the global Python environment.'''
+
+x = 5 + 2
+print(x)
